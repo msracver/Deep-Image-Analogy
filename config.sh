48f136e27723287620b0174a96d3715164a3deae
rozentill <rozentill@gmail.com>
2017-05-30 17:31:41 +0800

    add config file

---
+++ config.sh
@@ -0,0 +1,7 @@
+cd src/
+
+protoc caffe/proto/caffe.proto --cpp_out=../include/
+
+cd ..
+
+wget -c http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel -P example/deep_image_analogy/models/vgg19

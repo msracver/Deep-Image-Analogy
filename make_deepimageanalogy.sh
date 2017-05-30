96b29191f6d766d69d257cc9bacd6a836007cb51
rozentill <rozentill@gmail.com>
2017-05-30 20:58:43 +0800

    add make script

---
+++ make_deepimageanalogy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+source=example/deep_image_analogy/source
+
+nvcc $source/*.cpp $source/*.cu -o deep_image_analogy \
+	-std=c++11 \
+	-I./include \
+	-L./build/lib \
+	-lopencv_core \
+	-lopencv_highgui \
+	-lopencv_imgproc \
+	-lboost_system \
+	-lcublas \
+	-lcaffe \
+	-lglog 
